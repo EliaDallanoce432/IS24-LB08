f785d2698829e84b402c1066c8af0c9828a75797
SimonePizzelli <simone.pizzelli@mail.polimi.it>
2024-04-10 21:09:24 +0200

    Created GoldCardFeatherStrategyTest, tested some basic cases

---
+++ src/test/java/it/polimi/ingsw/server/model/GoldCardStrategy/GoldCardFeatherStrategyTest.java
@@ -0,0 +1,71 @@
+package it.polimi.ingsw.server.model.GoldCardStrategy;
+
+import it.polimi.ingsw.server.model.GameField;
+import it.polimi.ingsw.server.model.Player;
+import it.polimi.ingsw.server.model.card.GoldCard;
+import it.polimi.ingsw.server.model.card.ResourceCard;
+import it.polimi.ingsw.server.model.card.StarterCard;
+import it.polimi.ingsw.util.customexceptions.CannotPlaceCardException;
+import it.polimi.ingsw.util.supportclasses.Color;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GoldCardFeatherStrategyTest {
+
+    @Test
+    void calculatePointsCase1() {
+        //carta gold piazzabile e unico punto dato da feather che possiede la gold stessa
+        GameField gamefield = new GameField(new Player("player", Color.black));
+        gamefield.place(new StarterCard(82), true);
+        try {
+            gamefield.place(new ResourceCard(23), true, 1,1);
+            gamefield.place(new ResourceCard(12), true, -1,-1);
+            gamefield.place(new ResourceCard(1), true, 1,-1);
+            gamefield.place(new GoldCard(41),true,-1,1);
+
+        } catch (CannotPlaceCardException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(1,gamefield.getPlayer().getScore());
+    }
+
+    @Test
+    void calculatePointsCase2() {
+        //carta gold piazzabile e 1 feather già sul tavolo
+        GameField gamefield = new GameField(new Player("player", Color.black));
+        gamefield.place(new StarterCard(82), true);
+        try {
+            gamefield.place(new ResourceCard(23), true, 1,1);
+            gamefield.place(new ResourceCard(12), true, -1,-1);
+            gamefield.place(new ResourceCard(1), true, 1,-1);
+            gamefield.place(new ResourceCard(5), true, 2,0);
+            gamefield.place(new GoldCard(41),true,-1,1);
+
+        } catch (CannotPlaceCardException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(2,gamefield.getPlayer().getScore());
+    }
+
+    @Test
+    void calculatePointsCase3() {
+        //carta gold piazzabile e copre la feather già sul tavolo
+        GameField gamefield = new GameField(new Player("player", Color.black));
+        gamefield.place(new StarterCard(82), true);
+        try {
+            gamefield.place(new ResourceCard(23), true, 1,1);
+            gamefield.place(new ResourceCard(12), true, -1,-1);
+            gamefield.place(new ResourceCard(1), true, 1,-1);
+            gamefield.place(new ResourceCard(5), true, 2,0);
+            gamefield.place(new GoldCard(41),true,3,1);
+
+        } catch (CannotPlaceCardException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(1,gamefield.getPlayer().getScore());
+    }
+}
