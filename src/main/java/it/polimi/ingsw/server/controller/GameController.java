4f49022caac146fc713fa133b34a104d07a8ccd5
Simone Pizzelli <simone.pizzelli@mail.polimi.it>
2024-04-21 21:41:46 +0200

    Fixed gamecontroller class

--- src/main/java/it/polimi/ingsw/server/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/server/controller/GameController.java
@@ -3,19 +3,20 @@ package it.polimi.ingsw.server.controller;
 import it.polimi.ingsw.network.ClientHandler;
 import it.polimi.ingsw.network.ClientObserver;
 import it.polimi.ingsw.network.Lobby;
+import it.polimi.ingsw.server.model.Game;
 import org.json.simple.JSONObject;
 
 import java.util.ArrayList;
 
 public class GameController implements Runnable, ClientObserver {
     private String gameName;
-    private int numberOfPlayers;
+    private int numberOfExpectedPlayers;
     private ArrayList<ClientHandler> players;
     private Lobby lobby;
-
+    private Game game;
     public GameController(Lobby lobby, int numberOfPlayers, String gameName) {
         this.players = new ArrayList<>();
-        this.numberOfPlayers = numberOfPlayers;
+        this.numberOfExpectedPlayers = numberOfPlayers;
         this.lobby = lobby;
         System.out.println(gameName + " is ready");
     }
@@ -25,7 +26,7 @@ public class GameController implements Runnable, ClientObserver {
     }
 
     public int getNumberOfPlayers() {
-        return numberOfPlayers;
+        return numberOfExpectedPlayers;
     }
 
     public synchronized void enterGame(ClientHandler player) {
@@ -51,7 +52,7 @@ public class GameController implements Runnable, ClientObserver {
 
     }
 
-    private void gamePreparation(){
+    private void gamePreparation()  {
 
     }
 
---
+++ src/main/java/it/polimi/ingsw/server/controller/GameControllerRequestExecutor.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.server.controller;
+
+import it.polimi.ingsw.network.ClientHandler;
+import org.json.simple.JSONObject;
+
+public class GameControllerRequestExecutor {
+
+    public static void execute (GameController game, JSONObject message, ClientHandler clienthandler) {
+
+    }
+}
