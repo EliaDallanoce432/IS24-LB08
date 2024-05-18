bcde92b6828dfc88b09862b2332da3a978e88bab
Marco Pedretti <marco3.pedretti@mail.polimi.it>
2024-05-18 21:05:01 +0200

    Generated Javadoc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,15 +5,20 @@ module LB08 {
     requires json.simple;
     requires org.controlsfx.controls;
 
+    exports it.polimi.ingsw;
     exports it.polimi.ingsw.server.lobby;
+    exports it.polimi.ingsw.server.model;
+    exports it.polimi.ingsw.server.controller;
+    exports it.polimi.ingsw.server.view;
+    exports it.polimi.ingsw.client.model;
     exports it.polimi.ingsw.client.controller;
     exports it.polimi.ingsw.client.view;
-    exports it.polimi.ingsw;
-    exports it.polimi.ingsw.client.view.viewControllers;
-    exports it.polimi.ingsw.client.view.viewControllers.utility;
+    exports it.polimi.ingsw.network;
+    exports it.polimi.ingsw.util.customexceptions;
+    exports it.polimi.ingsw.util.supportclasses;
 
-    opens it.polimi.ingsw.client.view;
-    opens it.polimi.ingsw.client.controller to javafx.fxml;
-    opens it.polimi.ingsw.client.view.viewControllers;
-    opens it.polimi.ingsw.client.view.viewControllers.utility;
+//    opens it.polimi.ingsw.client.view;
+//    opens it.polimi.ingsw.client.controller to javafx.fxml;
+//    opens it.polimi.ingsw.client.view.viewControllers;
+//    opens it.polimi.ingsw.client.view.viewControllers.utility;
 }
