fbd136da0fc6953c9c8c7e4a12c6a29f8983c52c
Simone Pizzelli <simone.pizzelli@mail.polimi.it>
2024-05-21 16:35:00 +0200

    Added commands to ClientTerminalParser

--- src/main/java/it/polimi/ingsw/client/view/CLI/ClientTerminalParser.java
+++ src/main/java/it/polimi/ingsw/client/view/CLI/ClientTerminalParser.java
@@ -1,9 +1,7 @@
 package it.polimi.ingsw.client.view.CLI;
 
 import it.polimi.ingsw.client.controller.ClientController;
-import it.polimi.ingsw.client.model.PlayerModel;
 import it.polimi.ingsw.util.cli.CommandParser;
-import org.json.simple.JSONObject;
 
 public class ClientTerminalParser implements CommandParser {
 
@@ -15,13 +13,21 @@ public class ClientTerminalParser implements CommandParser {
             case "help", "?" -> help();
             case "setUsername" -> updateUsername(tokens);
             case "exit" -> ClientController.getInstance().shutdown();
-            case "createGame" -> createGame();
+            case "createGame" -> createGame(tokens);
             case "getAvailableGames" -> getAvailableGames();
             case "joinGame" -> joinGame(tokens);
             case "enterGame" -> enterGame(tokens);
             case "setReady" -> setReady();
             case "selectStarterCard" -> selectStarterCardOrientation(tokens);
             case "selectSecreteObjective" -> selectSecretObjective(tokens);
+            case "place" -> place(tokens);
+            case "directDrawResource" -> directDrawResource();
+            case "drawLeftResource" -> drawLeftResource();
+            case "drawRightResource" -> drawRightResource();
+            case "directDrawGold" -> directDrawGold();
+            case "drawLeftGold" -> drawLeftGold();
+            case "drawRightGold" -> drawRightGold();
+
         }
     }
 
@@ -36,8 +42,10 @@ public class ClientTerminalParser implements CommandParser {
     private void help() {
         //TODO stampare help
     }
-    private void createGame() {
-
+    private void createGame(String[] tokens) {
+        if (tokens.length == 3) {
+            ClientController.getInstance().sendSetUpGameMessage(tokens[1], Integer.parseInt(tokens[2]));
+        }
     }
 
     private void getAvailableGames () {
@@ -45,7 +53,7 @@ public class ClientTerminalParser implements CommandParser {
     }
 
     private void joinGame(String[] tokens) {
-
+        if (tokens.length == 2) ClientController.getInstance().sendJoinGameMessage(tokens[1]);
     }
     private void enterGame(String[] tokens) {
         if (tokens.length == 2) {
@@ -78,4 +86,36 @@ public class ClientTerminalParser implements CommandParser {
         }
     }
 
+    private void place(String[] tokens) {
+        if (tokens.length == 5) {
+            ClientController.getInstance().sendPlaceMessage(Integer.parseInt(tokens[1]),Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]), Boolean.parseBoolean(tokens[4]));
+        }
+        else {
+            //TODO parse error
+        }
+    }
+
+    private void directDrawResource() {
+        ClientController.getInstance().sendDirectDrawResourceCardMessage();
+    }
+
+    private void drawLeftResource() {
+        ClientController.getInstance().sendDrawLeftResourceCardMessage();
+    }
+
+    private void drawRightResource() {
+        ClientController.getInstance().sendDrawRightResourceCardMessage();
+    }
+
+    private void directDrawGold() {
+        ClientController.getInstance().sendDirectDrawGoldCardMessage();
+    }
+
+    private void drawLeftGold() {
+        ClientController.getInstance().sendDrawLeftGoldCardMessage();
+    }
+
+    private void drawRightGold() {
+        ClientController.getInstance().sendDrawRightGoldCardMessage();
+    }
 }
