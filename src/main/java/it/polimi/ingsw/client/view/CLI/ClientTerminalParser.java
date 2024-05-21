9762c65e6e510f62adc9279f0e9ce259adec4e10
Marco Pedretti <135032858+10736964@users.noreply.github.com>
2024-05-21 12:09:34 +0200

    Initial ClientTerminalParser implementation

--- src/main/java/it/polimi/ingsw/client/view/CLI/ClientTerminalParser.java
+++ src/main/java/it/polimi/ingsw/client/view/CLI/ClientTerminalParser.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.client.view.CLI;
 
+import it.polimi.ingsw.client.controller.ClientController;
 import it.polimi.ingsw.util.cli.CommandParser;
 
 public class ClientTerminalParser implements CommandParser {
@@ -7,7 +8,25 @@ public class ClientTerminalParser implements CommandParser {
 
     @Override
     public void parse(String command) {
-        System.out.println("prova");
-
+        String[] tokens = command.split("\\s+");
+        switch (tokens[0]) {
+            case "help": {
+                //TODO stampare help
+                break;
+            }
+            case "setUsername" : {
+                if (tokens.length == 2) {
+                    ClientController.getInstance().sendSetUsernameMessage(tokens[1]);
+                }
+                else {
+                    //TODO parse error
+                }
+                break;
+            }
+            case "exit": {
+                ClientController.getInstance().shutdown();
+                break;
+            }
+        }
     }
 }
