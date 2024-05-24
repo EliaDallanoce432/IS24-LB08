6d24f8202357fdb198d4c45197e57de8bc791b59
EliaDallanoce432 <135166322+EliaDallanoce432@users.noreply.github.com>
2024-05-24 14:50:06 +0200

    Add commands case of Server View

--- src/main/java/it/polimi/ingsw/server/view/ServerTerminalParser.java
+++ src/main/java/it/polimi/ingsw/server/view/ServerTerminalParser.java
@@ -2,6 +2,9 @@ package it.polimi.ingsw.server.view;
 
 import it.polimi.ingsw.util.cli.CommandParser;
 
+import static it.polimi.ingsw.util.supportclasses.Constants.INVALID_ARGUMENT_COUNT_MESSAGE;
+import static it.polimi.ingsw.util.supportclasses.Constants.INVALID_ARGUMENT_TYPE_MESSAGE;
+
 /**
  * This class parses the commands imparted to the server by the user through the terminal.
  */
@@ -12,76 +15,93 @@ public class ServerTerminalParser implements CommandParser {
         String[] tokens = command.split("\\s+");
         tokens[0] = tokens[0].toLowerCase();
         switch (tokens[0]) {
-            case "help": {
-                if (tokens.length == 1) {
-                    ServerView.getInstance().printHelp();
-                }
-                else ServerView.getInstance().parseError();
-                break;
-            }
-            case "echo": {
-                tokens[1] = tokens[1].toLowerCase();
-                if (tokens.length == 2) {
-                    if (tokens[1].equals("on")) {
-                        ServerView.getInstance().setEcho(true);
-                    } else if (tokens[1].equals("off")) {
-                        ServerView.getInstance().setEcho(false);
-                    }
-                    else ServerView.getInstance().parseError();
-                }
-                else ServerView.getInstance().parseError();
-                break;
-            }
-            case "setport": {
-                if (tokens.length == 1) {
-                    ServerView.getInstance().setPort(12345);
-                }
-                else if (tokens.length == 2) {
-                    try {
-                        ServerView.getInstance().setPort(Integer.parseInt(tokens[1]));
-                    } catch (NumberFormatException e) {
-                        System.out.println("The port number provided isn't an integer!");
-                        System.out.println();
-                    }
-                }
-                else ServerView.getInstance().parseError();
-                break;
-            }
-            case "port": {
-                if (tokens.length == 1) {
-                    ServerView.getInstance().showPort();
-                }
-                else ServerView.getInstance().parseError();
-                break;
-            }
-            case "clients": {
-                if (tokens.length == 1) {
-                    ServerView.getInstance().showConnectedClients();
-                }
-                else ServerView.getInstance().parseError();
-                break;
-            }
-            case "games": {
-                if (tokens.length == 1) {
-                    ServerView.getInstance().showGames();
-                }
-                else if(tokens.length == 3 && tokens[1].equals("--info")) {
-                    ServerView.getInstance().showGameInfo(tokens[2]);
-                }
-                else ServerView.getInstance().parseError();
-                break;
-            }
-            case "shutdown": {
-                if (tokens.length == 1) {
-                    ServerView.getInstance().shutdown();
-                }
-                else ServerView.getInstance().parseError();
-                break;
-            }
-            default: {
-                ServerView.getInstance().parseError();
-                break;
-            }
+            case "help" -> handleHelp(tokens);
+            case "echo" -> handleEcho(tokens);
+            case "setport" -> handleSetPort(tokens);
+            case "port" -> handlePort(tokens);
+            case "clients" -> handleClients(tokens);
+            case "games" -> handleGames(tokens);
+            case "shutdown" -> handleShutdown(tokens);
+            default -> ServerView.getInstance().parseError();
         }
+    }
+
+    private void handleHelp(String[] tokens) {
+        if (tokens.length > 1) {
+            System.out.println(INVALID_ARGUMENT_COUNT_MESSAGE);
+            return;
+        }
+        ServerView.getInstance().printHelp();
+    }
+
+    private void handleEcho(String[] tokens) {
+        if (tokens.length != 2) {
+            System.out.println(INVALID_ARGUMENT_COUNT_MESSAGE);
+            return;
+        }
+        String argument = tokens[1].toLowerCase();
+        if (!argument.equals("on") && !argument.equals("off")) {
+            System.out.println(INVALID_ARGUMENT_TYPE_MESSAGE);
+            return;
+        }
+        ServerView.getInstance().setEcho(argument.equals("on"));
+    }
+
+    private void handleSetPort(String[] tokens) {
+        if (tokens.length == 1) {
+            ServerView.getInstance().setPort(12345);
+        } else if (tokens.length == 2) {
+            try {
+                int port = Integer.parseInt(tokens[1]);
+                if (port <= 0 || port > 65535) {
+                    System.out.println("Invalid port number: must be between 1 and 65535");
+                    return;
+                }
+                ServerView.getInstance().setPort(port);
+            } catch (NumberFormatException e) {
+                System.out.println(INVALID_ARGUMENT_TYPE_MESSAGE);
+            }
+        } else {
+            System.out.println(INVALID_ARGUMENT_COUNT_MESSAGE);
+        }
+    }
+
+    private void handlePort(String[] tokens) {
+        if (tokens.length > 1) {
+            System.out.println(INVALID_ARGUMENT_COUNT_MESSAGE);
+            return;
+        }
+        ServerView.getInstance().showPort();
+    }
+
+    private void handleClients(String[] tokens) {
+        if (tokens.length > 1) {
+            System.out.println(INVALID_ARGUMENT_COUNT_MESSAGE);
+            return;
+        }
+        ServerView.getInstance().showConnectedClients();
+    }
+
+    private void handleGames(String[] tokens) {
+        if (tokens.length == 1) {
+            ServerView.getInstance().showGames();
+        } else if (tokens.length == 3 && tokens[1].equals("--info")) {
+            String gameName = tokens[2];
+            if (gameName.isEmpty()) {
+                System.out.println("Please provide a game name");
+                return;
+            }
+            ServerView.getInstance().showGameInfo(gameName);
+        } else {
+            System.out.println(INVALID_ARGUMENT_COUNT_MESSAGE);
+        }
+    }
+
+    private void handleShutdown(String[] tokens) {
+        if (tokens.length > 1) {
+            System.out.println(INVALID_ARGUMENT_COUNT_MESSAGE);
+            return;
+        }
+        ServerView.getInstance().shutdown();
     }
 }
--- src/main/java/it/polimi/ingsw/util/supportclasses/Constants.java
+++ src/main/java/it/polimi/ingsw/util/supportclasses/Constants.java
@@ -2,9 +2,11 @@ package it.polimi.ingsw.util.supportclasses;
 
 public final class Constants {
     public static int MAX_HAND_SIZE = 3;
-
+ //CLI Constants
     public static String MENU_HEADER =
             ConsoleColor.RED+"╔╦╗"+ ConsoleColor.GREEN+"╔═╗"+ ConsoleColor.PURPLE+"╔╗╔"+ConsoleColor.YELLOW+"╦ ╦\n" +
                     ConsoleColor.RED+"║║║"+ ConsoleColor.GREEN+"║╣ "+ ConsoleColor.PURPLE+"║║║"+ConsoleColor.YELLOW+"║ ║\n" +
                     ConsoleColor.RED+"╩ ╩"+ ConsoleColor.GREEN+"╚═╝"+ ConsoleColor.PURPLE+"╝╚╝"+ConsoleColor.YELLOW+"╚═╝\n"+ConsoleColor.RESET;
+    public static final String INVALID_ARGUMENT_COUNT_MESSAGE = "Invalid number of arguments";
+    public static final String INVALID_ARGUMENT_TYPE_MESSAGE = "Invalid argument type";
 }
